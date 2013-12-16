654044d25f8695d53244ab34d683e2c1e1d4222c
Dominik <dominik@dobots.nl>
2013-12-16 17:44:23 +0100

    initial commit

---
+++ src/org/dobots/aimrobotlibrary/AimRobotActivity.java
@@ -0,0 +1,102 @@
+package org.dobots.aimrobotlibrary;
+
+import org.dobots.aim.AimConnectionHelper;
+import org.dobots.aim.IAimModule;
+import org.dobots.utilities.BaseActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Message;
+import android.os.Messenger;
+import android.util.Log;
+
+public abstract class AimRobotActivity extends BaseActivity implements IAimModule {
+
+	// TODO: adjustable id, multiple modules
+	protected int mModuleId = 0; 
+
+	protected AimConnectionHelper mAimConnectionHelper;
+
+	@Override
+	public int getModuleId() {
+		// TODO Auto-generated method stub
+		return mModuleId;
+	}
+
+	public String getTag() {
+		return getModuleName();
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		Intent intent = getIntent();
+		mModuleId = intent.getIntExtra("id", -1);
+		
+		mAimConnectionHelper = new AimConnectionHelper(this);
+		mAimConnectionHelper.bindToMsgService();
+		
+		Log.i(getTag(),"onCreate");
+	}
+	
+	@Override
+	public void onAimStop() {
+		runOnUiThread(new Runnable() {
+			
+			@Override
+			public void run() {
+				finish();
+			}
+		});
+	}
+	
+	@Override
+	public void onStart() {
+		super.onStart();
+		Log.i(getTag(),"onStart");
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		Log.i(getTag(),"onResume");
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		Log.i(getTag(),"onPause");
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+		Log.i(getTag(),"onStop");
+	}
+
+	@Override
+	public void onDestroy() {
+		super.onDestroy();		
+		mAimConnectionHelper.destroy();
+		Log.i(getTag(), "onDestroy ");
+	}
+	
+	public Messenger getOutMessenger(String port) {
+		return mAimConnectionHelper.getOutMessenger(port);
+	}
+
+	public Messenger getInMessenger(String port) {
+		return mAimConnectionHelper.getInMessenger(port);
+	}
+
+	public void msgSend(Messenger messenger, Message msg) {
+		mAimConnectionHelper.msgSend(messenger, msg);
+	}
+	
+	public void sendData(Messenger messenger, String data) {
+		mAimConnectionHelper.sendData(messenger, data);
+	}
+
+}
+
---
+++ src/org/dobots/aimrobotlibrary/AimRobotService.java
@@ -0,0 +1,164 @@
+package org.dobots.aimrobotlibrary;
+
+import java.util.HashMap;
+
+import org.dobots.aim.AimProtocol;
+import org.dobots.aim.AimService;
+import org.dobots.lib.comm.msg.RoboCommands;
+import org.dobots.lib.comm.msg.RoboCommands.BaseCommand;
+import org.dobots.lib.comm.msg.RoboCommands.ControlCommand;
+import org.dobots.utilities.ThreadMessenger;
+import org.dobots.zmq.ZmqHandler;
+import org.dobots.zmq.video.FpsCounter;
+import org.dobots.zmq.video.IFpsListener;
+import org.dobots.zmq.video.IRawVideoListener;
+import org.dobots.zmq.video.VideoThrottle;
+import org.dobots.zmq.video.ZmqVideoReceiver;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.zeromq.ZMQ;
+
+import robots.remote.RobotServiceBinder;
+import robots.remote.RobotServiceBinder.RobotBinder;
+import android.content.Intent;
+import android.os.IBinder;
+import android.os.Message;
+import android.os.Messenger;
+import android.util.Log;
+
+public abstract class AimRobotService extends AimService implements IRawVideoListener {
+	
+	private static final String TAG = "RobotService";
+	
+	private RobotServiceBinder mRobot;
+	
+	// a ThreadMessenger is a thread with a messenger. messages are
+	// handled by that thread
+	// a normal Messenger uses the thread that creates the messenger
+	// to handle messages (which in this case would be the main (UI)
+	// thread
+	private ThreadMessenger mPortCmdInReceiver = new ThreadMessenger("PortCmdInMessenger") {
+		
+		@Override
+		public boolean handleIncomingMessage(Message msg) {
+			switch (msg.what) {
+			case AimProtocol.MSG_PORT_DATA:
+				// do we need to check datatype to make sure it is string?
+				String data = msg.getData().getString("data");
+				BaseCommand cmd = RoboCommands.decodeCommand(data);
+				handleData(cmd);
+				break;
+			default:
+				return false;
+			}
+			return true;
+		}
+	};
+	
+	protected void handleData(BaseCommand cmd) {
+		try {
+			if ((cmd instanceof ControlCommand) && ((ControlCommand)cmd).mCommand.equals("setFrameRate")) {
+				mVideoThrottle.setFrameRate((Double)((ControlCommand)cmd).getParameter(0));
+			} else {
+				mRobot.handleCommand(cmd);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	protected VideoThrottle mVideoThrottle;
+
+	private ZmqVideoReceiver mVideoReceiver;
+
+	private FpsCounter mCounter;
+	
+	@Override
+	public String getTag() {
+		return TAG;
+	}
+	
+	public void defineInMessenger(HashMap<String, Messenger> list) {
+		list.put("cmd", mPortCmdInReceiver.getMessenger());
+	}
+
+	public void defineOutMessenger(HashMap<String, Messenger> list) {
+		list.put("video", null);
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		Log.i(TAG, "onBind: " + intent.toString());
+		return mRobot.getBinder();
+	}
+	
+	public void onCreate() {
+		super.onCreate();
+		
+		ZmqHandler.initialize(this);
+		
+		mVideoThrottle = new VideoThrottle("videoThrottle");
+		mVideoThrottle.setRawVideoListener(this);
+		mVideoThrottle.setFrameRate(20.0);
+
+		mCounter = new FpsCounter(new IFpsListener() {
+			
+			@Override
+			public void onFPS(int i_nFPS) {
+				Log.d("debug", "fps: " + i_nFPS);
+			}
+		});
+	}
+	
+	@Override
+	public void onDestroy() {
+		super.onDestroy();
+		
+		if (mRobot != null) {
+			mRobot.destroy();
+		}
+		
+		mPortCmdInReceiver.destroy();
+		
+		mVideoThrottle.destroy();
+		mVideoReceiver.destroy();
+
+		ZmqHandler.destroyInstance();
+	}
+	
+
+	@Override
+	public void onFrame(byte[] rgb, int rotation) {
+		if (getOutMessenger("video") != null) {
+			String base64 = android.util.Base64.encodeToString(rgb, android.util.Base64.NO_WRAP);
+			
+			JSONObject json = new JSONObject();
+			try {
+				json.put("base64", base64);
+				json.put("rotation", rotation);
+
+				mAimConnectionHelper.sendData(getOutMessenger("video"), json.toString());
+				mCounter.tick();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
+	}
+
+	protected void setRobot(RobotServiceBinder robotServiceBinder) {
+		mRobot = robotServiceBinder;
+
+		ZMQ.Socket oVideoRecvSocket = ZmqHandler.getInstance().obtainVideoRecvSocket();
+		oVideoRecvSocket.subscribe(((RobotBinder)mRobot.getBinder()).getRobot().getID().getBytes());
+
+		mVideoReceiver = new ZmqVideoReceiver(oVideoRecvSocket);
+		mVideoReceiver.setRawVideoListener(mVideoThrottle);
+		mVideoReceiver.start();
+	}
+
+	protected RobotServiceBinder getRobot() {
+		return mRobot;
+	}
+
+}
